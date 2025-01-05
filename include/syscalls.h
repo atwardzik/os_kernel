bcbadf2b5225c6de1a1af8ae9a447e0c6ff53d0a
Artur Twardzik <polym.t@protonmail.com>
2025-01-05 13:04:12 +0100

    feat: add basic syscalls codes

---
+++ include/syscalls.h
@@ -0,0 +1,23 @@
+//
+// Created by Artur Twardzik on 05/01/2025.
+//
+
+#ifndef SYSCALLS_H
+#define SYSCALLS_H
+
+#define RESTART_SVC     0
+#define EXIT_SVC        1
+#define FORK_SVC        2
+#define READ_SVC        3
+#define WRITE_SVC       4
+#define OPEN_SVC        5
+#define CLOSE_SVC       6
+#define WAIT_PID_SVC    7
+#define GET_TIME_SVC    8
+#define GET_PID_SVC     9
+#define GET_PPID_SVC    10
+#define YIELD_SVC       11
+#define KILL            12
+
+
+#endif //SYSCALLS_H
