36fd2933d3d11e88a088b0ddf51ee8bfe1db46af
Artur Twardzik <polym.t@protonmail.com>
2025-01-06 00:21:24 +0100

    feat(process): implement process creation

--- include/proc.h
+++ include/proc.h
@@ -7,7 +7,19 @@
 
 #include "regs.h"
 #include "memory.h"
+#include "syscalls.h"
+
 #include <stddef.h>
+
+/*
+* The context switch routine has to:
+* - Manually stack remaining registers r4-r11 on the Process Stack
+* - Save current task’s PSP to memory
+* - Load next task’s stack pointer and assign it to PSP
+* - Manually unstack registers r4-r11
+* - Call bx 0xfffffffD which makes the processor switch to Unprivileged Handler Mode,
+*   unstack next task’s exception frame and continue on its PC.
+ */
 
 enum State {
         running,
@@ -17,7 +29,9 @@ enum State {
 
 typedef int pid_t;
 
+//TODO: by using MPU forbid process to access system resources
 struct Process {
+        void *ptr;
         void *pstack;
         pid_t pid;
         enum State pstate;
@@ -25,8 +39,6 @@ struct Process {
 };
 
 void scheduler_init(void);
-
-void svcall(void);
 
 pid_t create_process(void (*process_start_ptr)(void));
 
--- src/proc.c
+++ src/proc.c
@@ -4,16 +4,72 @@
 
 #include "proc.h"
 
+static constexpr size_t MAX_PROCESS_NUMBER = 20; //TODO: change to dynamic processes count
+static constexpr int HASH_MODULO = 3; //has to be coprime with MAX_PROCESS_NUMBER
+static constexpr int DELETED_TRACER = MAX_PROCESS_NUMBER * 2;
+static size_t pid_hash[MAX_PROCESS_NUMBER];
+
+static constexpr int DEFAULT_PROCESS_SIZE = 3 * 1024; //3 [KB]
+
+#define THREAD_PSP_CODE = 0xfffffffd;
+
 static struct {
         struct Process *processes;
         size_t total_allocated_memory;
         size_t maximum_processes_size;
         pid_t current_task;
+        pid_t last_pid;
 } scheduler __attribute__ ((section (".data")));
 
+
+static size_t calculate_pid_hash(pid_t pid, size_t i) {
+        size_t index = pid & 20; //nonnegative modulo?
+
+        if (!pid_hash[index] || pid_hash[index] == DELETED_TRACER || pid == 0) {
+                pid_hash[index] = pid;
+                return index;
+        }
+
+        return calculate_pid_hash(pid + HASH_MODULO * i, i + 1);
+}
+
+static size_t get_pid_position(pid_t pid) {
+        return calculate_pid_hash(pid, 0);
+}
+
 void scheduler_init(void) {
-        scheduler.processes = kmalloc(sizeof(struct Process) * 4);
+        scheduler.processes = kmalloc(sizeof(struct Process) * MAX_PROCESS_NUMBER);
         scheduler.total_allocated_memory = 0;
         scheduler.maximum_processes_size = HEAP_SIZE - get_current_heap_size();
         scheduler.current_task = 0;
+        scheduler.last_pid = 0;
+}
+
+pid_t create_process(void (*process_start_ptr)(void)) {
+        pid_t pid = scheduler.last_pid;
+        scheduler.last_pid += 1;
+
+        size_t index = get_pid_position(pid);
+
+        void *process_page = kmalloc(DEFAULT_PROCESS_SIZE);
+        void *pstack = process_page + DEFAULT_PROCESS_SIZE;
+
+        /* Save special registers which will be restored on exc. return:
+           - XPSR: Default value (0x01000000)
+           - PC: Point to the handler function
+           - LR: Point to a function to be called when the handler returns */
+        *(uint32_t *) (pstack - 1) = 0x0100'0000;
+        *(uint32_t *) (pstack - 2) = (uint32_t) process_start_ptr;
+        *(uint32_t *) (pstack - 3) = (uint32_t) exit;
+        //further registers ...
+
+        pstack -= 16;
+        struct Process process = {process_page, pstack, pid, ready, DEFAULT_PROCESS_SIZE};
+        scheduler.processes[index] = process;
+
+        return pid;
+}
+
+void exit(int exit_code) {
+        exit_code;
 }
