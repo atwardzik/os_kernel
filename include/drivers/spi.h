2afd5906ac3dcf2e422127ff16b0a77413f8d921
Artur Twardzik <polym.t@protonmail.com>
2026-03-30 23:50:35 +0200

    refactor(spi): get rid of useless hardware block address determination; replace with assembly macro

--- include/drivers/spi.h
+++ include/drivers/spi.h
@@ -7,12 +7,10 @@
 
 #include <stdint.h>
 
-void spi_init(uint32_t spi_address, uint32_t prescaler, uint32_t post_divider, uint32_t operating_mode);
+void spi_init(uint32_t spi_block_number, uint32_t prescaler, uint32_t post_divider, uint32_t operating_mode);
 
-uint32_t spi_determine_block(int spi_block);
+void spi_tx(int spi_block_number, uint16_t data);
 
-void spi_tx(int spi_block, uint16_t data);
-
-uint16_t spi_rx(int spi_block);
+uint16_t spi_rx(int spi_block_number);
 
 #endif //OS_SPI_H
