08921a81a494d60c846f394fbba546283838417a
Artur Twardzik <polym.t@protonmail.com>
2026-06-03 20:24:07 +0200

    feat(memory): implement simple hard drive interface

---
+++ include/fs/hard_drive.h
@@ -0,0 +1,19 @@
+//
+// Created by Artur Twardzik on 03/06/2026.
+//
+
+#ifndef OS_HARD_DRIVE_H
+#define OS_HARD_DRIVE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct HardDriveOperations {
+        int (*read_block)(uint32_t block_number, size_t block_size, char *buffer);
+
+        int (*write_block)(uint32_t block_number, size_t block_size, char *buffer);
+
+        int (*deinit)(void);
+};
+
+#endif //OS_HARD_DRIVE_H
