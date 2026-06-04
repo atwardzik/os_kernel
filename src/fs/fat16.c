004d75d825433cbadfda596555670ce7bb32cc17
Artur Twardzik <polym.t@protonmail.com>
2026-06-04 17:13:28 +0200

    fix(fs): mount fat16 partition in the parent ramfs

--- src/fs/fat16.c
+++ src/fs/fat16.c
@@ -70,32 +70,43 @@ static struct FileOperations *i_fop;
 
 
 static struct VFS_Inode *FAT16_alloc_inode(struct SuperBlock *sb) {
-        if (sb->current_inode_count >= sb->max_inode_count - 1) {
-                sb->max_inode_count += 32;
-                sb->inode_table = krealloc(sb->inode_table, sb->max_inode_count);
-        }
-
         struct FAT16_Inode *inode = kmalloc(sizeof(*inode));
         memset(inode, 0, sizeof(*inode));
         inode->vfs_inode.i_op = i_op;
         inode->vfs_inode.i_fop = i_fop;
         inode->vfs_inode.i_sb = sb;
 
-        sb->inode_table[sb->current_inode_count] = inode; //todo: iterate through inode_table and find free place
-        sb->current_inode_count += 1;
-
         return (struct VFS_Inode *) inode;
 }
 
 static void FAT16_destroy_inode(struct VFS_Inode *) {}
 
-static struct Dentry *FAT16_lookup(struct VFS_Inode *parent, struct Dentry *file, unsigned int) {}
+static struct Dentry *FAT16_lookup(struct VFS_Inode *parent, struct Dentry *file, unsigned int) {
+        const auto sb = (struct FAT16_SuperBlock *) parent->i_sb;
+        auto sb_op = (struct FAT16_SuperBlockOperations *) sb->sb.s_op;
 
-static ssize_t FAT16_read(struct File *file, void *buf, size_t count, off_t file_offset) {}
+        return nullptr;
+}
+
+static ssize_t FAT16_read(struct File *file, void *buf, size_t count, off_t file_offset) {
+#if 0
+        const struct FAT16_Inode *inode = (struct FAT16_Inode *) file->f_inode;
+        const auto sb = (struct FAT16_SuperBlock *) file->f_inode->i_sb;
+        const auto sb_op = (struct FAT16_SuperBlockOperations *) sb->sb.s_op;
+
+        const uint16_t bytes_per_sector = sb->boot_record.bytes_per_sector;
+        const off_t cluster_offset = file_offset / bytes_per_sector;
+        const size_t block_size = count - (count % bytes_per_sector) + bytes_per_sector;
+        if (sb_op->hd_op.read_block(inode->first_cluster + cluster_offset, block_size, buf, count)) {
+                file->f_pos += count;
+        }
+#endif
+        return 0;
+}
 
 static struct FAT16_BootRecord read_boot_sector(const uint32_t block_number, const struct HardDriveOperations *hd_op) {
         char buf[512];
-        hd_op->read_block(block_number, 512, buf);
+        hd_op->read_block(block_number, 512, buf, 512);
 
         const struct FAT16_BootRecord boot_record = *(struct FAT16_BootRecord *) buf;
 
@@ -107,6 +118,15 @@ static unsigned int disk_num = 0;
 struct Dentry *FAT16_mount(
         struct Dentry *parent_dir, const uint32_t block_number, const struct HardDriveOperations *hd_op
 ) {
+        // static operations struct for future inode use
+        i_op = kmalloc(sizeof(*i_op));
+        i_op->lookup = FAT16_lookup;
+        i_op->create = nullptr;
+
+        i_fop = kmalloc(sizeof(*i_fop));
+        i_fop->read = FAT16_read;
+        i_fop->write = nullptr;
+
         //filesystem structure
         struct FAT16_SuperBlockOperations *sb_op = kmalloc(sizeof(*sb_op));
         sb_op->s_op.alloc_inode = FAT16_alloc_inode;
@@ -117,18 +137,22 @@ struct Dentry *FAT16_mount(
         sb->sb.name = "fat16";
         sb->sb.s_op = (struct SuperBlockOperations *) sb_op;
 
-        sb->sb.current_inode_count = 0;
-        sb->sb.max_inode_count = 32;
-        sb->sb.inode_table = kmalloc(sizeof(void *) * sb->sb.max_inode_count);
         const auto boot_record = read_boot_sector(block_number, hd_op);
         memcpy(&sb->boot_record, &boot_record, sizeof(boot_record));
 
-        struct VFS_Inode *root_inode = FAT16_alloc_inode((struct SuperBlock *) sb);
-        root_inode->i_mode |= S_IFDIR;
-        root_inode->parent = parent_dir->inode;
+        struct FAT16_Inode *root_inode = (struct FAT16_Inode *) FAT16_alloc_inode((struct SuperBlock *) sb);
+        root_inode->vfs_inode.i_mode = S_IFDIR;
+        root_inode->vfs_inode.parent = parent_dir->inode;
+        root_inode->vfs_inode.i_fop = i_fop;
+        root_inode->vfs_inode.i_op = i_op;
+        root_inode->first_cluster = block_number
+                                    + boot_record.reserved_sectors
+                                    + boot_record.FAT_copies * boot_record.sectors_per_FAT;
+        root_inode->vfs_inode.i_size = boot_record.max_root_dentries * 32;
+
         struct Dentry *root = kmalloc(sizeof(*root));
         root->name = "/";
-        root->inode = root_inode;
+        root->inode = (struct VFS_Inode *) root_inode;
         root->sb = &sb->sb;
 
         sb->sb.s_root = root;
@@ -140,18 +164,9 @@ struct Dentry *FAT16_mount(
         strcat(name, itoa((int) disk_num, num, 10));
         disk_num += 1;
 
-        struct Dentry parent_dir_entry = {.name = name};
-
+        struct Dentry parent_dir_entry = {.name = name, .inode = (struct VFS_Inode *) root_inode};
         parent_dir->inode->i_op->create(parent_dir->inode, &parent_dir_entry, S_IFDIR | 0755);
 
-        // static operations struct for future inode use
-        i_op = kmalloc(sizeof(*i_op));
-        i_op->lookup = FAT16_lookup;
-        i_op->create = nullptr;
-
-        i_fop = kmalloc(sizeof(*i_fop));
-        i_fop->read = FAT16_read;
-        i_fop->write = nullptr;
 
         return root;
 }
